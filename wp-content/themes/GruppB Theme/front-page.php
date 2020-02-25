0a3e0ce8d09f26953f0f02abeb0783b638daa2ad
Mathias Beckius <lillmatte@hotmail.com>
2020-02-25 21:58:42 +0100

    front page design + fixes

--- wp-content/themes/GruppB Theme/front-page.php
+++ wp-content/themes/GruppB Theme/front-page.php
@@ -1,108 +1,124 @@
 <?php
 
 // Exit if accessed directly.
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 get_header();
 
-$container = get_theme_mod('understrap_container_type');
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php if (is_front_page() && is_home()) : ?>
-	<?php get_template_part('global-templates/hero'); ?>
+<?php if ( is_front_page() && is_home() ) : ?>
+	<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
 
 <div class="wrapper container-fluid" id="index-wrapper">
 
-	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
 			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part('global-templates/left-sidebar-check'); ?>
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<main class="site-main" id="main">
+			<main class="site-main content" id="main">
 				<h1>Utvalda bostäder</h1>
-				<?php if (have_posts()) : ?>
+				<?php if ( have_posts() ) : ?>
 
 					<?php
-					//Prepare query
+					// Prepare query
 					$loop = new WP_Query(
 						array(
 							'post_type' => 'property',
 						)
 					);
 
-					//Första loopen för att skriva ut utvalda
-					if ($loop->have_posts()) :
-						//Do WP_Loop if we get results
-						while ($loop->have_posts()) : $loop->the_post();
-							$imgurl = get_field('image', $post->ID);
+					// Första loopen för att skriva ut utvalda
+					if ( $loop->have_posts() ) :
+						?>
+					<div class="row mb-3">
+						<?php
+						// Do WP_Loop if we get results
+						while ( $loop->have_posts() ) :
+							$loop->the_post();
+							$imgurl = get_field( 'image', $post->ID );
 
-							if (filter_var($imgurl, FILTER_VALIDATE_URL) === FALSE) {
-								$imgurl = wp_get_attachment_url($imgurl);
+							if ( filter_var( $imgurl, FILTER_VALIDATE_URL ) === false ) {
+								$imgurl = wp_get_attachment_url( $imgurl );
 							}
 
-							if (get_field('utvalt_objekt') == true) { ?>
-									<div class="card" style="width: 20rem;">
-								<img class="card-img-top" src="<?php echo $imgurl ?>" alt="Card image cap">
+							if ( get_field( 'utvalt_objekt' ) == true ) {
+								?>
+							<div class="card col-md mr-3" style="width: 20rem;">
+								<img class="card-img-top" src="<?php echo $imgurl; ?>" alt="Card image cap">
 								<div class="card-body">
 									<h5 class="card-title"><?php echo get_the_title(); ?></h5>
-									<h5 class="card-text"><?php echo get_field('Address', $post->ID); ?></h5>
+									<h5 class="card-text"><?php echo get_field( 'Address' ); ?></h5>
 									<p class="card-text"> <?php the_content(); ?></p>
 									<a href="#" class="btn btn-primary">Gå till bostad</a>
 								</div>
 							</div>
 							<div class="pindex">
-								<?php if (has_post_thumbnail()) {
-									echo "has thumbnail"; ?>
+								<?php
+								if ( has_post_thumbnail() ) {
+									echo 'has thumbnail';
+									?>
 									<div class="pimage">
 										<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 									</div>
 								<?php } ?>
-							<?php }  ?>
+							</div>	
+							<?php } ?>
 					
 							<?php endwhile; ?>
-							<h2>Övriga bostäder</h1>
-							<?php //Andra loopen för att skriva ut resten `?>
+					</div>
+					<h2>Övriga bostäder</h1>
+					<div class="row mt-3">		
+							<?php // Andra loopen för att skriva ut resten ` ?>
 							<?php
-						//Do WP_Loop if we get results
-						while ($loop->have_posts()) : $loop->the_post();
-							$imgurl = get_field('image', $post->ID);
+							// Do WP_Loop if we get results
+							while ( $loop->have_posts() ) :
+								$loop->the_post();
+								$imgurl = get_field( 'image' );
 
-							if (filter_var($imgurl, FILTER_VALIDATE_URL) === FALSE) {
-								$imgurl = wp_get_attachment_url($imgurl);
-							}
+								if ( filter_var( $imgurl, FILTER_VALIDATE_URL ) === false ) {
+									$imgurl = wp_get_attachment_url( $imgurl );
+								}
 
-							if (get_field('utvalt_objekt') !== true) { ?>
-									<div class="card" style="width: 14rem;">
-								<img class="card-img-top" src="<?php echo $imgurl ?>" alt="Card image cap">
+								if ( get_field( 'utvalt_objekt' ) !== true ) {
+									?>
+							<div class="card col-md mr-3" style="width: 14rem;">
+								<img class="card-img-top" src="<?php echo $imgurl; ?>" alt="Card image cap">
 								<div class="card-body">
 									<h5 class="card-title"><?php echo get_the_title(); ?></h5>
-									<h5 class="card-text"><?php echo get_field('Address', $post->ID); ?></h5>
+									<h5 class="card-text"><?php echo get_field( 'Address' ); ?></h5>
 									<p class="card-text"> <?php the_content(); ?></p>
 									<a href="#" class="btn btn-primary">Gå till bostad</a>
 								</div>
 							</div>
 							<div class="pindex">
-								<?php if (has_post_thumbnail()) {
-									echo "has thumbnail"; ?>
+									<?php
+									if ( has_post_thumbnail() ) {
+										echo 'has thumbnail';
+										?>
 									<div class="pimage">
 										<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 									</div>
-								<?php } ?>
-							<?php }?>
-
-							<?php endwhile; ?>
-
+									<?php } ?>
 							</div>
+							<?php } ?>
+								<?php endwhile; ?>
+					</div>
+							
 							<?php
-							if ($loop->max_num_pages > 1) : ?>
+							if ( $loop->max_num_pages > 1 ) :
+								?>
 								<div id="nav-below" class="navigation">
-									<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Previous', 'domain')); ?></div>
-									<div class="nav-next"><?php previous_posts_link(__('Next <span class="meta-nav">&rarr;</span>', 'domain')); ?></div>
+									<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Previous', 'domain' ) ); ?></div>
+									<div class="nav-next"><?php previous_posts_link( __( 'Next <span class="meta-nav">&rarr;</span>', 'domain' ) ); ?></div>
 								</div>
-						<?php endif;
+								<?php
+						endif;
 						endif;
 						wp_reset_postdata();
 
@@ -111,12 +127,12 @@ $container = get_theme_mod('understrap_container_type');
 						* If you want to override this in a child theme, then include a file
 						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						*/
-						//get_template_part('loop-templates/content', get_post_format());
-						?>
+						// get_template_part('loop-templates/content', get_post_format());
+					?>
 
 					<?php else : ?>
 					
-						<?php get_template_part('loop-templates/content', 'none'); ?>
+						<?php get_template_part( 'loop-templates/content', 'none' ); ?>
 
 					<?php endif; ?>
 
@@ -124,14 +140,15 @@ $container = get_theme_mod('understrap_container_type');
 
 
 			<!-- The pagination component -->
-			<?php //understrap_pagination(); 
+			<?php
+			// understrap_pagination();
 			?>
 
 			<!-- Do the right sidebar check -->
-			<?php get_template_part('global-templates/right-sidebar-check'); ?>
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-		</div><!-- .row -->
-
+			</div><!-- .row -->
+			
 	</div><!-- #content -->
 
 </div><!-- #index-wrapper -->
--- wp-content/themes/GruppB Theme/single-property.php
+++ wp-content/themes/GruppB Theme/single-property.php
@@ -4,113 +4,126 @@
 */
 
 // Exit if accessed directly.
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 get_header();
-$container = get_theme_mod('understrap_container_type');
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="single-wrapper">
 
-    <div class="<?= esc_attr($container); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-        <div class="row">
+		<div class="row">
 
-            <!-- Do the left sidebar check -->
-            <?php get_template_part('global-templates/left-sidebar-check'); ?>
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-            <main class="site-main" id="main">
+			<main class="site-main" id="main">
 
-                <?php while (have_posts()) : the_post(); ?>
-                    <h1 class="display-4"> <?php the_title(); ?> </h1>
-                    <?php
-                    $images = acf_photo_gallery('image', $post->ID);
-                    $counter = 0;
-                    if (count($images)) :
-                    ?>
-                        <!--Loop out images -->
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <?php
-                                foreach ($images as $image) {
-                                    if ($counter === 0) { ?>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="<?= $counter ?>" class="active"></li>
-                                    <?php } else { ?>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="<?= $counter ?>"></li>
-                                <?php }
-                                    $counter++;
-                                } ?>
-                            </ol>
-                            <div class="carousel-inner">
-                                <?php
-                                $counter = 0;
-                                foreach ($images as $image) :
-                                    $id = $image['id']; // The attachment id of the media
-                                    $full_image_url = $image['full_image_url']; //Full size image url
-                                    $url = $image['url']; //Goto any link when clicked
-                                    $title = $image["title"];
-                                ?>
-                                    <?php if ($counter === 0) { ?>
-                                        <div class="carousel-item active">
-                                            <img class="d-block w-100" src="<?= $full_image_url; ?>" alt="<?= $title; ?>" title="<?= $title; ?>">
-                                        </div>
-                                    <?php
-                                    } else { ?>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="<?= $full_image_url; ?>" alt="<?= $title; ?>" title="<?= $title; ?>">
-                                        </div>
-                                <?php }
-                                    $counter++;
-                                endforeach;
-                                ?>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    <?php
-                    endif; ?>
-                    <p class="property-p"> <?php the_content(); ?> </p>
-                    <ul class="list-group">
-                        <li class="list-group-item">Adress : <?= get_field('Address') ?></li>
-                        <li class="list-group-item">Visningsdatum <?= get_field('visning') ?></li>
-                        <li class="list-group-item">Antal Rum: <?= get_field('rooms') ?> </li>
-                        <li class="list-group-item">Boarea: <?= get_field('boarea') ?> </li>
-                        <li class="list-group-item">Utgångsbud: <?= get_field('utgangsbud') ?></li>
-                    </ul>
-                    <?php var_dump(get_field('utvalt_objekt')); ?>
-                    <?php if (get_field('utvalt_objekt') == true) {
-                        echo "print this objekt";
-                    } else {
-                        echo "dont print this objekt";
-                    } ?>
-                    <?php the_field('utvalt_objekt'); ?>
-                    <?php //understrap_post_nav(); 
-                    ?>
+				<?php
+				while ( have_posts() ) :
+					the_post();
+					?>
+					<h1 class="display-4"> <?php the_title(); ?> </h1>
+					<?php
+					$images  = acf_photo_gallery( 'image', $post->ID );
+					$counter = 0;
+					if ( count( $images ) ) :
+						?>
+						<!--Loop out images -->
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<?php
+								foreach ( $images as $image ) {
+									if ( $counter === 0 ) {
+										?>
+										<li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $counter; ?>" class="active"></li>
+									<?php } else { ?>
+										<li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $counter; ?>"></li>
+										<?php
+									}
+									$counter++;
+								}
+								?>
+							</ol>
+							<div class="carousel-inner">
+								<?php
+								$counter = 0;
+								foreach ( $images as $image ) :
+									$id             = $image['id']; // The attachment id of the media
+									$full_image_url = $image['full_image_url']; // Full size image url
+									$url            = $image['url']; // Goto any link when clicked
+									$title          = $image['title'];
+									?>
+									<?php if ( $counter === 0 ) { ?>
+										<div class="carousel-item active">
+											<img class="d-block w-100" src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
+										</div>
+										<?php
+									} else {
+										?>
+										<div class="carousel-item">
+											<img class="d-block w-100" src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
+										</div>
+										<?php
+									}
+									$counter++;
+								endforeach;
+								?>
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+						<?php
+					endif;
+					?>
+					<p class="property-p"> <?php the_content(); ?> </p>
+					<ul class="list-group">
+						<li class="list-group-item">Adress : <?php echo get_field( 'Address' ); ?></li>
+						<li class="list-group-item">Visningsdatum <?php echo get_field( 'visning' ); ?></li>
+						<li class="list-group-item">Antal Rum: <?php echo get_field( 'rooms' ); ?> </li>
+						<li class="list-group-item">Boarea: <?php echo get_field( 'boarea' ); ?> </li>
+						<li class="list-group-item">Utgångsbud: <?php echo get_field( 'utgangsbud' ); ?></li>
+					</ul>
+					<?php var_dump( get_field( 'utvalt_objekt' ) ); ?>
+					<?php
+					if ( get_field( 'utvalt_objekt' ) == true ) {
+						echo 'print this objekt';
+					} else {
+						echo 'dont print this objekt';
+					}
+					?>
+					<?php the_field( 'utvalt_objekt' ); ?>
+					<?php
+					// understrap_post_nav();
+					?>
 
-                    <?php
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if (comments_open() || get_comments_number()) :
-                        comments_template();
-                    endif;
-                    ?>
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
 
-                <?php endwhile; // end of the loop. 
-                ?>
+					<?php
+				endwhile; // end of the loop.
+				?>
 
-            </main><!-- #main -->
+			</main><!-- #main -->
 
-            <!-- Do the right sidebar check -->
-            <?php get_template_part('global-templates/right-sidebar-check'); ?>
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-        </div><!-- .row -->
+		</div><!-- .row -->
 
-    </div><!-- #content -->
+	</div><!-- #content -->
 
 </div><!-- #single-wrapper -->
 
