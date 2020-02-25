1bf3261c14785da8625e0af630c9c783bc0243f4
Jonas Gertz <geertzo@live.se>
2020-02-25 15:03:14 +0100

    single-property fil, child theme

---
+++ wp-content/themes/understrap-child/single-property.php
@@ -0,0 +1,60 @@
+<?php
+/*
+* Template Name: Propery
+*/
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+$container = get_theme_mod('understrap_container_type');
+?>
+
+<div class="wrapper" id="single-wrapper">
+
+    <div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
+
+        <div class="row">
+
+            <!-- Do the left sidebar check -->
+            <?php get_template_part('global-templates/left-sidebar-check'); ?>
+
+            <main class="site-main" id="main">
+
+                <?php while (have_posts()) : the_post(); ?>
+                   <h1 class="display-4"> <?php the_title(); ?> </h1>
+                    <img src="<?php the_field('image'); ?>">
+                   <p class="property-p"> <?php the_content(); ?> </p>
+                    <ul class="list-group">
+                        <li class="list-group-item">Adress : <?= get_field('Address') ?></li>
+                        <li class="list-group-item">Visningsdatum <?= get_field('visning') ?></li>
+                        <li class="list-group-item">Antal Rum: <?= get_field('rooms') ?> </li>
+                        <li class="list-group-item">Boarea: <?= get_field('boarea') ?> </li>
+                        <li class="list-group-item">Utgångsbud: <?= get_field('utgangsbud') ?></li>
+                    </ul>
+    
+
+                    <?php //understrap_post_nav(); ?>
+
+                    <?php
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if (comments_open() || get_comments_number()) :
+                        comments_template();
+                    endif;
+                    ?>
+
+                <?php endwhile; // end of the loop. 
+                ?>
+
+            </main><!-- #main -->
+
+            <!-- Do the right sidebar check -->
+            <?php get_template_part('global-templates/right-sidebar-check'); ?>
+
+        </div><!-- .row -->
+
+    </div><!-- #content -->
+
+</div><!-- #single-wrapper -->
+
+<?php get_footer(); ?>
