b7718b23d54ea9859d78db9beb94b3c784d8d07a
Mathias Beckius <lillmatte@hotmail.com>
2020-03-02 14:37:32 +0100

    add while loop to search template

--- wp-content/themes/GruppB Theme/loop-templates/content-search.php
+++ wp-content/themes/GruppB Theme/loop-templates/content-search.php
@@ -2,20 +2,22 @@
 	<?php
 	// Do WP_Loop if we get results
 	get_search_query();
-	the_post();
-	$imgid  = get_field( 'image' );
-	$imgurl = wp_get_attachment_image_src( $imgid, 'medium' )[0];
-	?>
-	<div class="card flex-row flex-wrap col-md-12 p-0 mt-3">
-		<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo $imgurl; ?>" alt="Image of property"></a>
-		<div class="card-block p-2">
-			<a href="<?php echo get_the_permalink(); ?>">
-				<h4 class="card-title"><?php echo get_the_title(); ?></h4>
-			</a>
-			<p class="card-text">Adress : <?php echo get_field( 'address' ); ?></p>
-			<p class="card-text">Utgångsbud : <?php echo get_field( 'utgangsbud' ) . ' kr '; ?></p>
-			<p class="card-text">BoArea : <?php echo get_field( 'boarea' ) . 'm2 '; ?></p>
-			<p class="card-text">Antal rum : <?php echo get_field( 'rooms' ) . ' rum '; ?></p>
+	while ( have_posts() ) :
+		the_post();
+		$imgid  = get_field( 'image' );
+		$imgurl = wp_get_attachment_image_src( $imgid, 'medium' )[0];
+		?>
+		<div class="card flex-row flex-wrap col-md-12 p-0 mt-3">
+			<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo $imgurl; ?>" alt="Image of property"></a>
+			<div class="card-block p-2">
+				<a href="<?php echo get_the_permalink(); ?>">
+					<h4 class="card-title"><?php echo get_the_title(); ?></h4>
+				</a>
+				<p class="card-text">Adress : <?php echo get_field( 'address' ); ?></p>
+				<p class="card-text">Utgångsbud : <?php echo get_field( 'utgangsbud' ) . ' kr '; ?></p>
+				<p class="card-text">BoArea : <?php echo get_field( 'boarea' ) . 'm2 '; ?></p>
+				<p class="card-text">Antal rum : <?php echo get_field( 'rooms' ) . ' rum '; ?></p>
+			</div>
 		</div>
-	</div>
+	<?php endwhile; ?>
 </div>
