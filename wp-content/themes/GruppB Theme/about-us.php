90006986825bdd1fb6892370fe0bfc51aea255d8
Jonas Gertz <Geertzo@live.se>
2020-03-02 13:01:47 +0100

    added about-us template

---
+++ wp-content/themes/GruppB Theme/about-us.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+* Template Name: About-us
+*/
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+
+$container = get_theme_mod('understrap_container_type');
+?>
+
+<?php if (is_front_page() && is_home()) : ?>
+	<?php get_template_part('global-templates/hero'); ?>
+<?php endif; ?>
+
+<div class="wrapper container-fluid" id="index-wrapper">
+
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<div class="col-md content-area" id="primary">
+
+				<main class="site-main content" id="main">
+					
+					<h1>Om oss test</h1>
+					<?php
+					if(have_posts()) {
+						the_post();
+						the_content();
+					}
+ 
+					?>
+				</main><!-- #main -->
+
+
+				<!-- The pagination component -->
+				<?php
+
+				understrap_pagination();
+				?>
+			</div>
+			<!-- Do the right sidebar check -->
+			<?php get_template_part('sidebar-templates/sidebar-primary', 'right'); ?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #index-wrapper -->
+
+<?php get_footer(); ?>
