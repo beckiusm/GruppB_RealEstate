da9c3ff644c32cec08ec3562cc9bbc78ba05f0c9
Mathias Beckius <lillmatte@hotmail.com>
2020-03-02 16:43:13 +0100

    styling cards

--- wp-content/themes/GruppB Theme/loop-templates/content-search.php
+++ wp-content/themes/GruppB Theme/loop-templates/content-search.php
@@ -8,7 +8,7 @@
 		$imgurl = wp_get_attachment_image_src( $imgid, 'medium' )[0];
 		?>
 		<div class="card flex-row flex-wrap col-md-12 p-0 mb-3">
-			<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo $imgurl; ?>" alt="Image of property"></a>
+			<a href="<?php echo get_the_permalink(); ?>"><img class="loop-image" src="<?php echo $imgurl; ?>" alt="Image of property"></a>
 			<div class="card-block p-2">
 				<a href="<?php echo get_the_permalink(); ?>">
 					<h4 class="card-title"><?php echo get_the_title(); ?></h4>
